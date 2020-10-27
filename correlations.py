a4f77514c94cfdf141e5d3aec636d32278376a9f
Ilmo Heiska <ilmo.heiska@cinia.fi>
2020-10-27 20:50:07 +0200

    satisfaction scores calculated

---
+++ correlations.py
@@ -0,0 +1,60 @@
+import math
+import pandas as pd
+
+# calculate Pearson Correlation value between the target user and candidate users
+# returns a dataframe with columns PearsonCorr and userId
+def pearson_correlations(targetUserRatings, similarUserCandidates, correlationThreshold):
+    pearsonCorrelationDict = {}
+
+    for userId, ratings in similarUserCandidates:
+        ratings = ratings.sort_values(by='movieId')
+        targetUserRatings = targetUserRatings.sort_values(by='movieId')
+
+        numberOfRatings = len(ratings)
+        moviesInCommon = targetUserRatings[targetUserRatings['movieId'].isin(
+            ratings['movieId'].tolist())]
+        ratingsOfMoviesInCommonByTargetUser = moviesInCommon['rating'].tolist()
+        ratingsOfOtherUser = ratings['rating'].tolist()
+
+        Sxx = sum([i**2 for i in ratingsOfMoviesInCommonByTargetUser]) - \
+            pow(sum(ratingsOfMoviesInCommonByTargetUser), 2) / \
+            float(numberOfRatings)
+        Syy = sum([i**2 for i in ratingsOfOtherUser]) - \
+            pow(sum(ratingsOfOtherUser), 2)/float(numberOfRatings)
+        Sxy = sum(i*j for i, j in zip(ratingsOfMoviesInCommonByTargetUser, ratingsOfOtherUser)) - \
+            sum(ratingsOfMoviesInCommonByTargetUser) * \
+            sum(ratingsOfOtherUser)/float(numberOfRatings)
+
+        if Sxx != 0 and Syy != 0:
+            pearsonCorrelationDict[userId] = Sxy/math.sqrt(Sxx*Syy)
+        else:
+            pearsonCorrelationDict[userId] = 0
+
+    # create a correlation dataframe and sort according to the correlation value (descending)
+    correlationsDF = pd.DataFrame.from_dict(
+        pearsonCorrelationDict, orient='index')
+    correlationsDF.columns = ['PearsonCorr']
+    correlationsDF['userId'] = correlationsDF.index
+    correlationsDF.index = range(len(correlationsDF))
+    correlationsDF.sort_values(by='PearsonCorr', ascending=False, inplace=True)
+
+    # NOTE check if necessary..
+    # finally, remove those users, that have a correlation value lower than threshold
+    return correlationsDF[correlationsDF['PearsonCorr'] > correlationThreshold]
+
+# get similar users for target user (userId)
+# function will return max 'n' users
+def similar_users(df, userId, maxSimilarUsers, correlationThreshold):
+    print('\ntarget user: ', userId)
+    targetUserRatings = df[df['userId'] == userId]
+
+    print('\nsubset of users that have watched the same movies as the target user')
+    condition1 = df['userId'] != userId
+    condition2 = df['movieId'].isin(targetUserRatings['movieId'].tolist())
+    userSubset = df[condition1 & condition2]
+
+    # sort in descending order
+    userSubsetSorted = sorted(userSubset.groupby(
+        ['userId']), key=lambda x: len(x[1]), reverse=True)
+
+    return pearson_correlations(targetUserRatings, userSubsetSorted[:maxSimilarUsers], correlationThreshold)
--- recommendations.py
+++ recommendations.py
@@ -1,71 +1,15 @@
 import pandas as pd
 import numpy as np
 import random
-import math
+
+from correlations import pearson_correlations, similar_users
+
+NUMBER_OF_USERS = 5
+CORRELATION_THRESHOLD = 0.2
+SIMILAR_USERS_MAX = 50
 
 ratingsDF = pd.read_csv('movielens-small/ratings.csv')
 ratingsDF.drop(['timestamp'], axis=1, inplace=True)
-moviesDF = pd.read_csv('movielens-small/movies.csv')
-
-# calculate Pearson Correlation value between the target user and candidate users
-# returns a dataframe with columns PearsonCorr and userId
-def pearson_correlations(targetUserRatings, similarUserCandidates):
-    pearsonCorrelationDict = {}
-
-    for userId, ratings in similarUserCandidates:
-        ratings = ratings.sort_values(by='movieId')
-        targetUserRatings = targetUserRatings.sort_values(by='movieId')
-
-        numberOfRatings = len(ratings)
-        moviesInCommon = targetUserRatings[targetUserRatings['movieId'].isin(
-            ratings['movieId'].tolist())]
-        ratingsOfMoviesInCommonByTargetUser = moviesInCommon['rating'].tolist()
-        ratingsOfOtherUser = ratings['rating'].tolist()
-
-        Sxx = sum([i**2 for i in ratingsOfMoviesInCommonByTargetUser]) - \
-            pow(sum(ratingsOfMoviesInCommonByTargetUser), 2) / \
-            float(numberOfRatings)
-        Syy = sum([i**2 for i in ratingsOfOtherUser]) - \
-            pow(sum(ratingsOfOtherUser), 2)/float(numberOfRatings)
-        Sxy = sum(i*j for i, j in zip(ratingsOfMoviesInCommonByTargetUser, ratingsOfOtherUser)) - \
-            sum(ratingsOfMoviesInCommonByTargetUser) * \
-            sum(ratingsOfOtherUser)/float(numberOfRatings)
-
-        if Sxx != 0 and Syy != 0:
-            pearsonCorrelationDict[userId] = Sxy/math.sqrt(Sxx*Syy)
-        else:
-            pearsonCorrelationDict[userId] = 0
-
-    # create a correlation dataframe and sort according to the correlation value (descending)
-    correlationsDF = pd.DataFrame.from_dict(
-        pearsonCorrelationDict, orient='index')
-    correlationsDF.columns = ['PearsonCorr']
-    correlationsDF['userId'] = correlationsDF.index
-    correlationsDF.index = range(len(correlationsDF))
-    correlationsDF.sort_values(by='PearsonCorr', ascending=False, inplace=True)
-
-    # NOTE check if necessary..
-    # finally, remove those users, that have a correlation value lower than threshold
-    correlationThreshold = 0.2
-    return correlationsDF[correlationsDF['PearsonCorr'] > correlationThreshold]
-
-# get similar users for target user (userId)
-# function will return max 'n' users
-def similar_users(df, userId, maxSimilarUsers):
-    print('\ntarget user: ', userId)
-    targetUserRatings = df[df['userId'] == userId]
-
-    print('\nsubset of users that have watched the same movies as the target user')
-    condition1 = df['userId'] != userId
-    condition2 = df['movieId'].isin(targetUserRatings['movieId'].tolist())
-    userSubset = df[condition1 & condition2]
-
-    # sort in descending order
-    userSubsetSorted = sorted(userSubset.groupby(
-        ['userId']), key=lambda x: len(x[1]), reverse=True)
-
-    return pearson_correlations(targetUserRatings, userSubsetSorted[:maxSimilarUsers])
-
 
 def calculate_recommendations(ratingsDF, correlationsDF, userId):
     # prediction function:
@@ -117,70 +61,98 @@ def calculate_recommendations(ratingsDF, correlationsDF, userId):
     # scale of ratings = tuple of (lowest rating, highest rating)
     ratingScale = (ratingScale[0], ratingScale[len(ratingScale) - 1])
     scaler = MinMaxScaler(feature_range=(ratingScale))
-    recommendationDF['recommendation score, scaled'] = scaler.fit_transform(recommendationDF['recommendation score'].values.reshape(-1,1))
+    recommendationDF['prediction for user ' + str(userId)] = scaler.fit_transform(recommendationDF['recommendation score'].values.reshape(-1,1))
     return recommendationDF
 
-# aggregation method: average
-def calculate_group_recommendation_list_average(recommendationLists):
+# group recommendation list with aggregation methods: average and least misery
+def calculate_group_recommendation_list(recommendationLists):
     tempGroupRecommendationDF = recommendations[0]
     
     for i in range(1, len(recommendationLists)):
         tempGroupRecommendationDF = tempGroupRecommendationDF.merge(recommendations[i], left_on='movieId', right_on='movieId', how='outer', suffixes=(i - 1, i))
 
-    columns = [col for col in tempGroupRecommendationDF.columns if 'recommendation score, scaled' in col or col == 'movieId']
+    columns = [col for col in tempGroupRecommendationDF.columns if 'prediction' in col or col == 'movieId']
     groupRecommendationDF = tempGroupRecommendationDF[columns]
 
     # remove rows with NaN values
     # in other words: we only consider the movies, that have a predicted score for each user in the group
     groupRecommendationDF = groupRecommendationDF.dropna()
-    print(groupRecommendationDF)
 
     # calculate the average score, and add new column
     groupRecommendationDF.insert(1, 'average', groupRecommendationDF.iloc[:, 1:].mean(axis=1))
     groupListSorted = groupRecommendationDF.sort_values(by=['average'], ascending=False)
+
+    # calculate the least misery score, and add new column
+    groupRecommendationDF.insert(2, 'least misery', groupRecommendationDF.iloc[:, 2:].min(axis=1))
+    groupListSorted = groupRecommendationDF.sort_values(by=['least misery'], ascending=False)
+
     return groupListSorted
 
-# aggregation method: least misery
-def calculate_group_recommendation_list_least_misery(recommendationLists):
+# hybrid group recommendation list
+def calculate_group_recommendation_list_hybrid(recommendationLists, alfa):
     tempGroupRecommendationDF = recommendations[0]
     
     for i in range(1, len(recommendationLists)):
         tempGroupRecommendationDF = tempGroupRecommendationDF.merge(recommendations[i], left_on='movieId', right_on='movieId', how='outer', suffixes=(i - 1, i))
 
-    columns = [col for col in tempGroupRecommendationDF.columns if 'recommendation score, scaled' in col or col == 'movieId']
+    columns = [col for col in tempGroupRecommendationDF.columns if 'prediction' in col or col == 'movieId']
     groupRecommendationDF = tempGroupRecommendationDF[columns]
 
     # remove rows with NaN values
     # in other words: we only consider the movies, that have a predicted score for each user in the group
     groupRecommendationDF = groupRecommendationDF.dropna()
 
+    # calculate the average score, and add new column
+    groupRecommendationDF.insert(1, 'average', groupRecommendationDF.iloc[:, 1:].mean(axis=1))
+
     # calculate the least misery score, and add new column
-    groupRecommendationDF.insert(1, 'least misery', groupRecommendationDF.iloc[:, 1:].min(axis=1))
-    groupListSorted = groupRecommendationDF.sort_values(by=['least misery'], ascending=False)
+    groupRecommendationDF.insert(2, 'least misery', groupRecommendationDF.iloc[:, 2:].min(axis=1))
+
+    if (alfa == 0):
+        groupRecommendationDF.insert(1, 'result', groupRecommendationDF['average'])
+
+    groupListSorted = groupRecommendationDF.sort_values(by=['result'], ascending=False)
     return groupListSorted
+
+def calculate_satisfaction(groupRecommendationList, users, k):
+    #resultGroup = groupRecommendationList[['movieId','result']][:10]
+    #resultGroup.index = np.arange(1, len(resultGroup) + 1)
+    #resultUsers = []
+    satisfaction = {}
+    for i in range(0, len(users)):
+        user = users[i]
+        column = [col for col in groupRecommendationList.columns if col == 'prediction for user ' + str(user)]
+        predictedScoreSumGroupList = groupRecommendationList[column][:k].sum()
+        predictedScoreSumOwnList = groupRecommendationList[column].sort_values(by=column[0], ascending=False)[:k].sum()
+        satisfaction[user] = predictedScoreSumGroupList.array[0] / predictedScoreSumOwnList.array[0]
+    print('satisfaction scores:')
+    print(satisfaction)
+    print('for group recommendation list:')
+    print(groupRecommendationList)
 
 #####################################
 
 allUsers = ratingsDF['userId'].unique().tolist()
 users = []
 # pick random users
-for i in range(0,4):
+for i in range(0, NUMBER_OF_USERS):
     user = random.choice(allUsers)
     users.append(user)
     allUsers.remove(user)
 
-# add individual recommendation lists to a list
+### ROUND 1
+# at first round, the average aggregation is implemented (alfa = 0)
+
+# calculate individual recommendation lists and add to a list
 recommendations = []
 for i in range(0,len(users)):
-    correlationsDF = similar_users(ratingsDF, users[i], 50)
+    correlationsDF = similar_users(ratingsDF, users[i], SIMILAR_USERS_MAX, CORRELATION_THRESHOLD)
     recommendations.append(calculate_recommendations(ratingsDF, correlationsDF, users[i]))
 
-# aggregate the group recommendation list
-groupList = calculate_group_recommendation_list_average(recommendations)
-print('\n RESULT: average aggregation')
+groupList = calculate_group_recommendation_list_hybrid(recommendations, 0)
+print('\n RESULT: hybrid')
 print(groupList)
 
-groupListLeastMisery = calculate_group_recommendation_list_least_misery(recommendations)
-print('\n RESULT: least misery aggregation')
-print(groupListLeastMisery)
-
+# calculate satisfaction scores, use only top-k items in the group recommendation list
+k = 10
+satisfaction = calculate_satisfaction(groupList, users, k)
