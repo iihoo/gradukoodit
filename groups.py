3ae7a08cca7ba35c3ca1308eb4cea3349d2a2cd8
Ilmo Heiska <ilmo.heiska@cinia.fi>
2020-11-25 16:11:02 +0200

    minor update to group type creation and add small set of sample groups

--- groups.py
+++ groups.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 
 import groupformation
 import similarities
@@ -11,24 +12,22 @@ DISSIMILARITY_THRESHOLD = -0.5
 # get initial data chunk 
 initialRatingsDataChunk = pd.read_csv('movielens-25m/ratings.csv', usecols=['userId', 'movieId', 'rating'], chunksize=INITIAL_DATA_CHUNK_SIZE)
 df_ratings_initial_chunk = initialRatingsDataChunk.get_chunk()
-'''
+
 # create file, return error if exists already
 with open('grouptypes/group-3-2.csv', 'x') as file:
     # write parameter info to file
     print(f'Parameters: data size = {INITIAL_DATA_CHUNK_SIZE}, movies in common minimum = {MOVIES_IN_COMMON_MINIMUM}', file=file)
 
     # create groups and append to file
-    for i in range(0, 1):
-        group = groupformation.create_group_type_3_2(df_ratings_initial_chunk, MOVIES_IN_COMMON_MINIMUM)
-        print(similarities.calculate_group_similarity_matrix(group, df_ratings_initial_chunk))
-        groupToString = ','.join([str(user) for user in group])
-        print(groupToString, file=file)
-'''
-
-for i in range(0, 3):
+    for i in range(0, 100):
+        start = time.time()
         group = groupformation.create_group_type_3_2(df_ratings_initial_chunk, MOVIES_IN_COMMON_MINIMUM, SIMILARITY_THRESHOLD, DISSIMILARITY_THRESHOLD)
+        end = time.time()
         if (group == None):
-            print(f'\nNo suitable users could be found at round {i + 1}.\n')
+            print(f'\nNo suitable users could be found at round {i + 1}.')
         else:
             print(similarities.calculate_group_similarity_matrix(group, df_ratings_initial_chunk))
+            group.sort()
             groupToString = ','.join([str(user) for user in group])
+            print(groupToString, file=file)
+        print(f'...took {round((end - start), 0)} seconds.\n')
