056f5cd3e027fb1713fe4cd75fe600311d647c86
Ilmo Heiska <ilmo.heiska@cinia.fi>
2020-11-26 11:01:56 +0200

    refactor recommendations: now possible to calculate results for several groups

--- recommendations.py
+++ recommendations.py
@@ -12,7 +12,19 @@ import visualization
 # just for wide printouts
 pd.set_option('display.expand_frame_repr', False)
 
-NUMBER_OF_USERS = 5
+groups = [[265, 390, 799, 8051, 10989],
+[265, 390, 799, 8051, 10989],
+[21, 91, 3001, 6562, 9840],
+[16, 57, 260, 4991, 6074],
+[2, 20, 2915, 4862, 6446],
+[5, 10, 1154, 6579, 9133],
+[22, 85, 1450, 4475, 7278],
+[57, 196, 251, 1056, 9685],
+[5, 675, 1207, 2597, 12856],
+[16, 127, 2715, 3012, 7959]]
+
+NUMBER_OF_USERS = len(groups[0])
+NUMBER_OF_ITERATIONS = len(groups)
 CORRELATION_THRESHOLD = 0.7
 MOVIES_IN_COMMON_MINIMUM = 6
 RECOMMENDATION_ROUNDS = 15
@@ -28,105 +40,99 @@ ratingScale.sort()
 ratingScale = (ratingScale[0], ratingScale[len(ratingScale) - 1])
 scaler = MinMaxScaler(feature_range=(ratingScale))
 
-users = [16, 127, 2715, 3012, 7959]
-
-# calculate similarity matrix for the group
-df_group_similarity = similarities.calculate_group_similarity_matrix(users, df_ratings_initial_chunk)
-
-print('\nCalculating recommendations...')
-start = time.time()
-
-# calculate individial recommendation lists (a dict, where userId is the key and the recommendation list for that user is the dict value)
-recommendations = calculations.calculate_recommendations_all(df_ratings_initial_chunk, scaler, users, MOVIES_IN_COMMON_MINIMUM, CORRELATION_THRESHOLD)
-
-# top-k movies
-k = 10
-
-# alfa variable for hybrid aggregation method
-alfa = 0
-
-# for the first round, initialize satisfaction score = 1, for each user (modified average aggregation)
-satisfactionModifiedAggregation = {u:1 for u in users}
-
-# create a DataFrame for satisfaction & dissatisfaction scores
-df_scores = pd.DataFrame(columns=['GroupSat:HYBRID', 'GroupSat:MODIF.AGGR.', 'GroupDis:HYBRID', 'GroupDis:MODIF.AGGR.'])
-
-# initialize list of removed movies for both methods
-removedMoviesHybrid = []
-removedMoviesModifiedAggregation = []
-
 # create a DataFrame for GroupSatO and GroupDisO results
-df_results = pd.DataFrame(columns=['GroupSatO:HYBRID', 'GroupSatO:MODIF.AGGR.', 'GroupDisO:HYBRID', 'GroupDisO:MODIF.AGGR.'])
+index = pd.MultiIndex.from_tuples([(1,5)], names=('group', 'round'))
+df_results = pd.DataFrame(index=index, columns=['GroupSatO:HYBRID', 'GroupSatO:MODIF.AGGR.', 'GroupDisO:HYBRID', 'GroupDisO:MODIF.AGGR.', 'F-score:HYBRID', 'F-score:MODIF.AGGR.'])
 
-# simulate recommendation rounds
-for i in range(1, RECOMMENDATION_ROUNDS + 1):
-    print('\n**********************************************************************************************************************************')
-    print(f'ROUND {i}')
+for i in range(0, NUMBER_OF_ITERATIONS):
+    # pick one group for this iteration
+    users = groups[i]
 
-    # calculate group recommendation list(s)
-    groupListHybrid = calculations.calculate_group_recommendation_list_hybrid(recommendations, alfa)
-    groupListModifiedAggregation = calculations.calculate_group_recommendation_list_modified_average_aggregation(recommendations, satisfactionModifiedAggregation)
+    # calculate similarity matrix for the group
+    df_group_similarity = similarities.calculate_group_similarity_matrix(users, df_ratings_initial_chunk)
 
-    # filter movies that have already been recommended in the previous rounds
-    groupListHybridResult = groupListHybrid[~groupListHybrid.movieId.isin(removedMoviesHybrid)]
-    groupListModifiedAggregationResult = groupListModifiedAggregation[~groupListModifiedAggregation.movieId.isin(removedMoviesModifiedAggregation)]
+    print(f'\nCalculating recommendations for group {i + 1}...')
+    start = time.time()
 
-    # calculate satisfaction scores, use only top-k items in the group recommendation list
-    satisfactionHybrid = calculations.calculate_satisfaction(groupListHybridResult, recommendations, k)
-    satisfactionModifiedAggregation = calculations.calculate_satisfaction(groupListModifiedAggregationResult, recommendations, k)
+    # calculate individial recommendation lists (a dict, where userId is the key and the recommendation list for that user is the dict value)
+    recommendations = calculations.calculate_recommendations_all(df_ratings_initial_chunk, scaler, users, MOVIES_IN_COMMON_MINIMUM, CORRELATION_THRESHOLD)
 
-    # modify alfa value (used in the hybrid method)
-    alfa = max(list(satisfactionHybrid.values())) - min(list(satisfactionHybrid.values()))
+    # top-k movies
+    k = 10
 
-    # calculate the average satisfaction scores from this round
-    groupSatHybrid = sum(satisfactionHybrid.values()) / len(satisfactionHybrid)
-    groupSatModifiedAggregation = sum(satisfactionModifiedAggregation.values()) / len(satisfactionModifiedAggregation)
+    # alfa variable for hybrid aggregation method
+    alfa = 0
 
-    # calculate the dissatisfaction scores from this round
-    groupDisHybrid = max(satisfactionHybrid.values()) - min(satisfactionHybrid.values())
-    groupDisModifiedAggregation = max(satisfactionModifiedAggregation.values()) - min(satisfactionModifiedAggregation.values())
+    # for the first round, initialize satisfaction score = 1, for each user (modified average aggregation)
+    satisfactionModifiedAggregation = {u:1 for u in users}
 
-    # add to results dataframe
-    df_scores.loc[i] = [groupSatHybrid, groupSatModifiedAggregation, groupDisHybrid, groupDisModifiedAggregation]
+    # create a DataFrame for satisfaction & dissatisfaction scores
+    df_scores = pd.DataFrame(columns=['GroupSat:HYBRID', 'GroupSat:MODIF.AGGR.', 'GroupDis:HYBRID', 'GroupDis:MODIF.AGGR.'])
 
-    # top-k results
-    print('\nResults, hybrid aggregation:')
-    print(groupListHybridResult[:k])
-    print('\nResults, modified aggregation:')
-    print(groupListModifiedAggregationResult[:k])
+    # initialize list of removed movies for both methods
+    removedMoviesHybrid = []
+    removedMoviesModifiedAggregation = []
 
-    # add top-k movies as to-be-removed, so as to not recommend same movies in the next round
-    removedMoviesHybrid.extend(groupListHybridResult['movieId'][:k].values)
-    removedMoviesModifiedAggregation.extend(groupListModifiedAggregationResult['movieId'][:k].values)
+    # simulate recommendation rounds
+    for r in range(1, RECOMMENDATION_ROUNDS + 1):
+        # calculate group recommendation list(s)
+        groupListHybrid = calculations.calculate_group_recommendation_list_hybrid(recommendations, alfa)
+        groupListModifiedAggregation = calculations.calculate_group_recommendation_list_modified_average_aggregation(recommendations, satisfactionModifiedAggregation)
 
-    # calculate results after 5, 10 and 15 rounds
-    if i in [5,10,15]:
-        # calculate average of the average of group satisfaction scores
-        groupSatOHybrid = df_scores['GroupSat:HYBRID'].mean()
-        groupSatOModifiedAggregation = df_scores['GroupSat:MODIF.AGGR.'].mean()
+        # filter movies that have already been recommended in the previous rounds
+        groupListHybridResult = groupListHybrid[~groupListHybrid.movieId.isin(removedMoviesHybrid)]
+        groupListModifiedAggregationResult = groupListModifiedAggregation[~groupListModifiedAggregation.movieId.isin(removedMoviesModifiedAggregation)]
 
-        # calculate average of the group dissatisfaction scores
-        groupDisOHybrid = df_scores['GroupDis:HYBRID'].mean()
-        groupDisOModifiedAggregation = df_scores['GroupDis:MODIF.AGGR.'].mean()
+        # calculate satisfaction scores, use only top-k items in the group recommendation list
+        satisfactionHybrid = calculations.calculate_satisfaction(groupListHybridResult, recommendations, k)
+        satisfactionModifiedAggregation = calculations.calculate_satisfaction(groupListModifiedAggregationResult, recommendations, k)
+
+        # modify alfa value (used in the hybrid method)
+        alfa = max(list(satisfactionHybrid.values())) - min(list(satisfactionHybrid.values()))
+
+        # calculate the average satisfaction scores from this round
+        groupSatHybrid = sum(satisfactionHybrid.values()) / len(satisfactionHybrid)
+        groupSatModifiedAggregation = sum(satisfactionModifiedAggregation.values()) / len(satisfactionModifiedAggregation)
+
+        # calculate the dissatisfaction scores from this round
+        groupDisHybrid = max(satisfactionHybrid.values()) - min(satisfactionHybrid.values())
+        groupDisModifiedAggregation = max(satisfactionModifiedAggregation.values()) - min(satisfactionModifiedAggregation.values())
 
         # add to results dataframe
-        df_results.loc[i] = [groupSatOHybrid, groupSatOModifiedAggregation, groupDisOHybrid, groupDisOModifiedAggregation]
+        df_scores.loc[r] = [groupSatHybrid, groupSatModifiedAggregation, groupDisHybrid, groupDisModifiedAggregation]
 
-print(f'\nAFTER {RECOMMENDATION_ROUNDS} RECOMMENDATION ROUNDS')
+        # add top-k movies as to-be-removed, so as to not recommend same movies in the next round
+        removedMoviesHybrid.extend(groupListHybridResult['movieId'][:k].values)
+        removedMoviesModifiedAggregation.extend(groupListModifiedAggregationResult['movieId'][:k].values)
 
-print('\nResults:')
-print(df_scores)
-print()
+        # calculate results after 5, 10 and 15 rounds
+        if r in [5,10,15]:
+            # calculate average of the average of group satisfaction scores
+            groupSatOHybrid = round(df_scores['GroupSat:HYBRID'].mean(), 3)
+            groupSatOModifiedAggregation = round(df_scores['GroupSat:MODIF.AGGR.'].mean(), 3)
+
+            # calculate average of the group dissatisfaction scores
+            groupDisOHybrid = round(df_scores['GroupDis:HYBRID'].mean(), 3)
+            groupDisOModifiedAggregation = round(df_scores['GroupDis:MODIF.AGGR.'].mean(), 3)
+
+            # calculate F-scores
+            F_hybrid = round(calculations.calculate_F_score(groupSatOHybrid, groupDisOHybrid), 3)
+            F_ModifiedAggregation = round(calculations.calculate_F_score(groupSatOModifiedAggregation, groupDisOModifiedAggregation), 3)
+
+            # add to results dataframe
+            df_results.loc[(i + 1,r),:] = [groupSatOHybrid, groupSatOModifiedAggregation, groupDisOHybrid, groupDisOModifiedAggregation, F_hybrid, F_ModifiedAggregation]
+
+    end = time.time()
+    print(f'..calculations finished in {(end - start):1f} seconds')
+    print(f'SIMILARITY MATRIX FOR USERS IN THE GROUP:')
+    print(df_group_similarity)
+
+    ## PLOT RESULTS
+    #visualization.plot_satisfaction_dissatisfaction(df_scores)
+
+print(f'\nRESULTS AFTER {NUMBER_OF_ITERATIONS} ITERATION ROUNDS')
 print(df_results)
 
-print(f'\nF-score, HYBRID: {calculations.calculate_F_score(groupSatOHybrid, groupDisOHybrid):4f}')
-print(f'F-score, MODIFIED AGGREGATION: {calculations.calculate_F_score(groupSatOModifiedAggregation, groupDisOModifiedAggregation):4f}')
 
-print('\nSIMILARITY MATRIX FOR TARGER USERS:')
-print(df_group_similarity)
 
-end = time.time()
-print(f'\nRecommendations finished in {(end - start):1f} seconds')
 
-### PLOT RESULTS
-visualization.plot_satisfaction_dissatisfaction(df_scores)
